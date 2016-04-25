4e60874e4eb9a5f862f547ccc8cb3676e55f513a
davpapp <david.papp@students.olin.edu>
2016-04-25 10:11:30 -0400

    implented mat2cell and dct2

--- linproject.m
+++ linproject.m
@@ -1,11 +1,23 @@
-I = imread('C:\Users\dpapp\Desktop\einstein.png','png');
-imshow(I);
-[width, height] = size(I)
-A1 = I(1:8, 1:8);
-for i = 1:width:8
-    for j = 1:height:8
-        block = I(i:i+8, j,j+8)
-        greyscale_block = 
+IMG = imread('C:\Users\dpapp\Documents\GitHub\linearity-final\einstein.png','png');
+IMG = rgb2gray(IMG);
+[width, height, dim] = size(IMG)
+
+blockSizeR = 8;
+blockSizeC = 8;
+
+wholeBlockRows = floor(width / blockSizeR);
+blockVectorR = [blockSizeR * ones(1, wholeBlockRows), rem(width, blockSizeR)]
+wholeBlockCols = floor(height / blockSizeC);
+blockVectorC = [blockSizeC * ones(1, wholeBlockCols), rem(height, blockSizeC)]
+
+if dim > 1
+    blocks = mat2cell(IMG, blockVectorR, blockVectorC, dim);
+else
+    blocks = mat2cell(IMG, blockVectorR, blockVectorC);
+end
+
+for i = 1:width/8
+    for j = 1:height/8
+        matDCT = dct2(blocks{i, j})
     end
 end
-imshow(A1);
